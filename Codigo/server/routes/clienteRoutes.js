263064dd696c3a8d7d7d766aefbb74b4808b1710
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-02 07:30:37 -0300

    att

--- Codigo/server/routes/clienteRoutes.js
+++ Codigo/server/routes/clienteRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express')
 const router = new Router();
 const cliente = require("../controllers/clienteController");
-const verificacao = require('../requireAuth');
+//const verificacao = require('../requireAuth');
 
 router.post("/cliente", verificacao, cliente.create);
 router.get("/oficina", verificacao, cliente.index);
-router.get("/clienteilterId/:id", verificacao, cliente.filterId)
+router.get("/clienteilterId/:id", verificacao, cliente.filterIdCliente)
 router.put("/clientePut/:id", verificacao, cliente.update);
 router.delete("/cliente/:id", verificacao, cliente.delete);
 
--- Codigo/server/routes/usuarioRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const router = new Router();
-const cliente = require("../controllers/usuarioController");
-const verificacao = require('../requireAuth');
-
-router.post("/usuario", verificacao, cliente.create);
-
-module.exports = router
