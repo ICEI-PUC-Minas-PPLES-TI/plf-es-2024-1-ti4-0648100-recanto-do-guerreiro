8768f97af3ac8b613dcaf2654d028f53330af883
João Gabriel Perez <104282903+JoaoGperez@users.noreply.github.com>
2024-04-07 18:10:25 -0300

    #14 add funcionalidade do botão editar em reservas

--- Codigo/client/js/reserva.js
+++ Codigo/client/js/reserva.js
@@ -1,14 +1,48 @@
-// Redirecionamento para a página novaReserva
+// variavel global
+var formData;
+
+// ouvinte de evento ao botão "Editar"
 document.querySelector('.btn-editar').addEventListener('click', function() {
-    window.location.href = 'novaReserva.html'; 
+    // Seletor para todos os campos de entrada e textareas
+    var campos = document.querySelectorAll('input, textarea');
+    
+    // Habilita a edição de todos os campos
+    campos.forEach(function(campo) {
+        campo.removeAttribute('disabled');
+    });
+
+    // Altera o texto do botão para "Salvar"
+    this.textContent = 'Salvar';
+
+    // Remove o ouvinte de evento de clique atual
+    this.removeEventListener('click', arguments.callee);
+
+    this.addEventListener('click', function() {
+        $.ajax({
+            type: 'PUT',
+            url: 'http://localhost:8000/reservaPut',
+            data: formData,
+            success: function(response) {
+                // Resposta de sucesso;
+                console.log('Data saved successfully:', response);
+            },
+            error: function(xhr, status, error) {
+                // Erro;
+                console.error('Error saving data:', error);
+            }
+        });
+        console.log('Dados editados serão enviados para o servidor');
+    });
 });
 
 // Exclusão da reserva ao clicar em "Excluir"
 document.querySelector('.btn-excluir').addEventListener('click', function() {
     if (confirm('Tem certeza que deseja excluir esta reserva?')) {
-        // Se o usuário confirmar, você pode implementar a lógica para excluir a reserva aqui
-        // Por exemplo, você pode fazer uma solicitação AJAX para um servidor para excluir a reserva do banco de dados
-        // Depois de excluir com sucesso, você pode redirecionar o usuário de volta para a página inicial ou fazer qualquer outra ação necessária
+        // Implemente aqui a lógica para excluir a reserva
+        // Por exemplo, você pode fazer uma solicitação AJAX para um servidor
+        // para excluir a reserva do banco de dados
+        // Após a exclusão bem-sucedida, você pode redirecionar o usuário de volta para a página inicial ou
+        // realizar qualquer outra ação necessária
         alert('Reserva excluída com sucesso!');
     }
 });
