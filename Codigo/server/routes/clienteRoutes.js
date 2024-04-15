776751f9d59178577891798b5868aec185055cc7
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-15 10:29:11 -0300

    att integracao reserva e cliente

--- Codigo/server/routes/clienteRoutes.js
+++ Codigo/server/routes/clienteRoutes.js
@@ -4,7 +4,7 @@ const cliente = require("../controllers/clienteController");
 const verificacao = require('../requireAuth');
 
 router.post("/cliente", verificacao, cliente.create);
-router.get("/oficina", verificacao, cliente.index);
+router.get("/cliente", verificacao, cliente.index);
 router.get("/clienteilterId/:id", verificacao, cliente.filterIdCliente)
 router.put("/clientePut/:id", verificacao, cliente.update);
 router.delete("/cliente/:id", verificacao, cliente.delete);
