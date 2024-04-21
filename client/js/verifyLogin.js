5d10dc8ccf04fab6765ea55c07cb3e70fb010615
Gabriel Alejandro Figueiro Galindo <83712515+gabriel6988@users.noreply.github.com>
2024-04-21 11:52:57 -0300

    Delete client directory

--- client/js/verifyLogin.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const currentUrl = window.location.href;
-    const token = sessionStorage.getItem('token')
-    if (!token) {
-        window.location.assign('./login.html')
-    }
-})
