82052fe1e6f9fa323d242753fac969c68f69849a
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-23 11:27:36 -0300

    att botao logout

--- Codigo/server/routes/authRoute.js
+++ Codigo/server/routes/authRoute.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-// Defina suas rotas
-router.get("/", (req, res) => {
-  res.send("Bem-vindo ao Recanto do Guerreiro!");
-});
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, senha } = req.body;
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ message: 'Usuário não encontrado' });
+      return res.status(401).json({ message: "Usuário não encontrado" });
     }
     const senhaCorreta = await bcrypt.compare(senha, user.senha);
     if (!senhaCorreta) {
-      return res.status(401).json({ message: 'Senha incorreta' });
+      return res.status(401).json({ message: "Senha incorreta" });
     }
-    const token = jwt.sign({ userId: user.id }, 'suaChaveSecreta', { expiresIn: '8h' });
+    const token = jwt.sign({ userId: user.id }, "suaChaveSecreta", {
+      expiresIn: "8h",
+    });
     return res.json({ token });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Erro interno do servidor' });
+    return res.status(500).json({ message: "Erro interno do servidor" });
   }
 });
 
