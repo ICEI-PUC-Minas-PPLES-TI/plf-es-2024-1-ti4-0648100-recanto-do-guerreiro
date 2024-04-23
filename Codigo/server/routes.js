ac2ab868f340f40d997dba3e5de8a2b8fe2f43a9
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-23 09:44:05 -0300

    att

--- Codigo/server/routes.js
+++ Codigo/server/routes.js
@@ -3,9 +3,9 @@ const router = new Router();
 const reservasRoutes = require("./routes/reservasRoutes");
 const clienteRoutes = require("./routes/clienteRoutes");
 const gestaoRoutes = require("./routes/gestaoRoutes");
-//const userRoutes = require('./routes/userRoutes')
+const userRoutes = require("./routes/userRoutes");
 //const authRoute = require('./routes/authRoute')
 
-router.use(reservasRoutes, clienteRoutes, gestaoRoutes);
+router.use(reservasRoutes, clienteRoutes, gestaoRoutes, userRoutes);
 
 module.exports = router;
