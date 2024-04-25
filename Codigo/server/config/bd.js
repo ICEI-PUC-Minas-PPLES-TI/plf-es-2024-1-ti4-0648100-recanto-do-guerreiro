0b36e0266b37d2f7803694e076ab7e94b19af32a
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-25 10:43:23 -0300

    att

--- Codigo/server/config/bd.js
+++ Codigo/server/config/bd.js
@@ -21,52 +21,52 @@
 
 //BANCO DE DADOS LOCAL
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 // Configurações de conexão com o banco de dados
-/*const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'Phmcf#2003',
-});*/
 const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'cacau69#Deus',
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "Phmcf#2003",
 });
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: 'localhost',
+//     username: 'root',
+//     password: 'cacau69#Deus',
+// });
 
 // Função para criar o banco de dados se não existir
 async function createDatabase() {
-    try {
-        await sequelize.query(`CREATE DATABASE IF NOT EXISTS recantodoguerreiro;`);
-        console.log('Banco de dados criado com sucesso ou já existente');
-    } catch (error) {
-        console.error('Erro ao criar banco de dados:', error);
-    }
+  try {
+    await sequelize.query(`CREATE DATABASE IF NOT EXISTS recantodoguerreiro;`);
+    console.log("Banco de dados criado com sucesso ou já existente");
+  } catch (error) {
+    console.error("Erro ao criar banco de dados:", error);
+  }
 }
 
 // Chama a função para criar o banco de dados
 createDatabase();
 
 // Exporta as configurações
-/*module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'Phmcf#2003',
-    database: 'recantodoguerreiro',
-    define: {
-        timestamp: true,
-    },*/
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: 'cacau69#Deus',
-    database: 'recantodoguerreiro',
-    define: {
-        timestamp: true,
-    },
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "Phmcf#2003",
+  database: "recantodoguerreiro",
+  define: {
+    timestamp: true,
+  },
+  // module.exports = {
+  //     dialect: 'mysql',
+  //     host: 'localhost',
+  //     username: 'root',
+  //     password: 'cacau69#Deus',
+  //     database: 'recantodoguerreiro',
+  //     define: {
+  //         timestamp: true,
+  //     },
 };
