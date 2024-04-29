b166974d00f7d56db16dd334a8c7ead021dbaaa4
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-29 07:42:59 -0300

    att

--- Codigo/server/routes/reservasRoutes.js
+++ Codigo/server/routes/reservasRoutes.js
@@ -3,29 +3,14 @@ const router = new Router();
 const reserva = require("../controllers/reservaController");
 const verificacao = require("../requireAuth");
 
-router.post("/reserva", verificacao, async (req, res) => {
-  const { data } = req.body;
-  const dataJaReservada = await reserva.verificarDataExistente(data);
-  if (dataJaReservada) {
-    return res.status(400).json({ mensagem: "Essa data já foi reservada." });
-  }
-  reserva.create(req, res);
-});
+router.post("/reserva", verificacao, reserva.create);
 router.get("/reserva", verificacao, reserva.index);
 router.get("/filterIdReserva/:id", verificacao, reserva.filterIdReserva);
-router.put("/reservaPut/:id", verificacao, async (req, res) => {
-  const reservaid = req.params.id;
-  const { data } = req.body;
-  const dataJaReservada = await reserva.verificarDataExistente(data, reservaid);
-  if (dataJaReservada) {
-    return res.status(400).json({ mensagem: "Essa data já foi reservada." });
-  }
-  reserva.update(req, res);
-});
+router.put("/reservaPut/:id", verificacao, reserva.update);
 router.put("/reservaStatus/:id", verificacao, reserva.updateStatus);
 router.delete("/reserva/:id", verificacao, reserva.delete);
 
 // Rota para verificar se uma data de reserva já existe
-router.get("/verificarData", verificacao, reserva.verificarData);
+//router.get("/verificarData", verificacao, reserva.verificarData);
 
 module.exports = router;
