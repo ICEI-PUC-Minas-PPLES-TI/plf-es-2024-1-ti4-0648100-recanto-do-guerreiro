e943f5bf128e7fb40a6f4b15ce3f02e5b3a6542f
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-29 07:57:21 -0300

    att

--- Codigo/server/controllers/reservaController.js
+++ Codigo/server/controllers/reservaController.js
@@ -4,104 +4,102 @@ const fs = require("fs").promises;
 const path = require("path");
 
 class reservaController {
-  async create(req, res) {
-    try {
-      const novaReserva = await reserva.create({
-        titulo: req.body.titulo,
-        descricao: req.body.descricao,
-        data: req.body.data,
-        hora: req.body.hora,
-        idCliente: req.body.idCliente,
-        adicionais: req.body.adicionais,
-        status: req.body.status,
-      });
-      return res.status(200).json(novaReserva);
-    } catch (erro) {
-      return res.status(500).json("Reserva não cadastrada" + erro);
-    }
-  }
+    async create(req, res) {
+        try {
+            const novaReserva = await reserva.create({
+                titulo: req.body.titulo,
+                descricao: req.body.descricao,
+                data: req.body.data,
+                hora: req.body.hora,
+                idCliente: req.body.idCliente,
+                adicionais: req.body.adicionais,
+                status: req.body.status,
+            });
 
-  async filter(req, res) {
-    try {
-      const { data, status } = req.query;
-      const reservaFil = await reserva.findAll({
-        where: {
-          data: data,
-          status: status,
-        },
-      });
-      console.log(reservaFil);
-      return res.status(200).json(reservaFil);
-    } catch (erro) {
-      return res.status(500).json("Erro ao encontrar a reserva" + erro);
-    }
-  }
+            return res.status(200).json(novaReserva);
 
-  async index(req, res) {
-    try {
-      const reservasImport = await reserva.findAll();
-      return res.status(200).json(reservasImport);
-    } catch (erro) {
-      return res.status(500).json("Erro ao encontrar as reservas" + erro);
+        } catch (erro) {
+            return res.status(500).json("Erro ao cadastrar a reserva: " + erro);
+        }
     }
-  }
 
-  async filterIdReserva(req, res) {
-    try {
-      const reservasFil = await reserva.findOne({
-        where: { id: req.params.id },
-      });
-      console.log(reservasFil);
-      return res.status(200).json(reservasFil);
-    } catch (erro) {
-      return res.status(500).json("Erro ao pegar as reservas" + erro);
+    async filter(req, res) {
+        try {
+            const { data, status } = req.query;
+            const reservaFil = await reserva.findAll({
+                where: {
+                    data: data,
+                    status: status,
+                },
+            });
+            console.log(reservaFil);
+            return res.status(200).json(reservaFil);
+        } catch (erro) {
+            return res.status(500).json("Erro ao encontrar a reserva" + erro);
+        }
     }
-  }
 
-  async updateStatus(req, res) {
-    try {
-      await reserva.update(
-        {
-          status: req.body.status,
-        },
-        { where: { id: req.params.id } }
-      );
-      return res.status(200).json("Sucesso ao atualizar a reserva");
-    } catch (erro) {
-      return res.status(500).json("Erro ao atualizar" + erro);
+    async index(req, res) {
+        try {
+            const reservasImport = await reserva.findAll();
+            return res.status(200).json(reservasImport);
+        } catch (erro) {
+            return res.status(500).json("Erro ao encontrar as reservas" + erro);
+        }
     }
-  }
 
-  async update(req, res) {
-    try {
-      await reserva.update(
-        {
-          titulo: req.body.titulo,
-          descricao: req.body.descricao,
-          data: req.body.data,
-          hora: req.body.hora,
-          idCliente: req.body.idCliente,
-          adicionais: req.body.adicionais,
-          status: req.body.status,
-        },
-        { where: { id: req.params.id } }
-      );
-      return res.status(200).json("Sucesso ao atualizar a reserva");
-    } catch (erro) {
-      return res.status(500).json("Erro ao atualizar" + erro);
+    async filterIdReserva(req, res) {
+        try {
+            const reservasFil = await reserva.findOne({
+                where: { id: req.params.id },
+            });
+            console.log(reservasFil);
+            return res.status(200).json(reservasFil);
+        } catch (erro) {
+            return res.status(500).json("Erro ao pegar as reservas" + erro);
+        }
     }
-  }
 
-  async delete(req, res) {
-    try {
-      await reserva.destroy({
-        where: { id: req.params.id },
-      });
-      return res.status(200).json("Sucesso ao deletar");
-    } catch (erro) {
-      return res.status(500).json("Erro ao deletar" + erro);
+    async updateStatus(req, res) {
+        try {
+            await reserva.update({
+                status: req.body.status,
+            }, { where: { id: req.params.id } });
+            return res.status(200).json("Sucesso ao atualizar a reserva");
+        } catch (erro) {
+            return res.status(500).json("Erro ao atualizar" + erro);
+        }
     }
-  }
+
+    async update(req, res) {
+        try {
+            await reserva.update({
+                titulo: req.body.titulo,
+                descricao: req.body.descricao,
+                data: req.body.data,
+                hora: req.body.hora,
+                idCliente: req.body.idCliente,
+                adicionais: req.body.adicionais,
+                status: req.body.status,
+            }, { where: { id: req.params.id } });
+
+            return res.status(200).json("Reserva atualizada com sucesso.");
+
+        } catch (erro) {
+            return res.status(500).json("Erro ao atualizar a reserva: " + erro);
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            await reserva.destroy({
+                where: { id: req.params.id },
+            });
+            return res.status(200).json("Sucesso ao deletar");
+        } catch (erro) {
+            return res.status(500).json("Erro ao deletar" + erro);
+        }
+    }
 }
 
 module.exports = new reservaController();
