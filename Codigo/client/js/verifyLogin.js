7c42c1c7e85db15085d573c43eafc708c7ef90eb
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-04-30 09:53:12 -0300

    att

--- Codigo/client/js/verifyLogin.js
+++ Codigo/client/js/verifyLogin.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", () => {
-  const currentUrl = window.location.href;
-  const token = sessionStorage.getItem("token");
-  if (!token) {
-    window.location.href = "../index.html";
-  }
+    const currentUrl = window.location.href;
+    const token = sessionStorage.getItem("token");
+    if (!token) {
+        window.alert("Voce precisa estar logado!");
+        window.location.href = "../index.html";
+    }
 });
