2b0ae067c4510d29ef8dd3dd11e331a411ab78ca
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-06-01 22:49:44 -0300

    att

--- Codigo/server/config/bd.js
+++ Codigo/server/config/bd.js
@@ -66,6 +66,7 @@ module.exports = {
 };*/
 
 // TESTE DE DEPLOY
+
 require('dotenv').config();
 
 module.exports = {
@@ -74,20 +75,20 @@ module.exports = {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: 'postgres', // Defina o dialeto como 'postgres'
+        dialect: process.env.DB_DIALECT,
     },
     test: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: 'postgres', // Defina o dialeto como 'postgres'
+        dialect: process.env.DB_DIALECT,
     },
     production: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
-        dialect: 'postgres', // Defina o dialeto como 'postgres'
+        dialect: process.env.DB_DIALECT,
     }
 };
